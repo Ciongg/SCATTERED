9197a137c7b2e185dfa7393305b53e914dc64c4c
Ciongg <karlinciong04@gmail.com>
2024-08-06 22:21:35 +0800

    Force System for Trash Spawn

--- Assets/Scripts/Objects.cs
+++ Assets/Scripts/Objects.cs
@@ -4,29 +4,36 @@ using UnityEngine;
 
 public class Objects : MonoBehaviour
 {
-
-    public Transform [] spawnPoints;
-    public GameObject [] prefabs;
-
+    public Transform[] spawnPoints;
+    public GameObject[] prefabs;
     public float timeBetweenSpawn;
+    public float minForce = 300f; // Minimum force
+    public float maxForce = 700f; // Maximum force
     float nextSpawnTime;
 
     private GameObject trash;
 
-
     // Update is called once per frame
     void Update()
     {
-        if(Time.time > nextSpawnTime){
-            spawnTrash();
+        if (Time.time > nextSpawnTime)
+        {
+            SpawnTrash();
             nextSpawnTime = Time.time + timeBetweenSpawn;
-
         }
-        
     }
 
-
-    void spawnTrash(){
-        trash = Instantiate(prefabs[Random.Range(0, prefabs.Length)], spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+    void SpawnTrash()
+    {
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
+        trash = Instantiate(prefabs[Random.Range(0, prefabs.Length)], spawnPoint.position, randomRotation);
+        Rigidbody2D rb = trash.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            float randomForce = Random.Range(minForce, maxForce);
+            Vector2 forceDirection = new Vector2(Random.Range(-0.5f, 0.5f), 1).normalized;
+            rb.AddForce(forceDirection * randomForce);
+        }
     }
 }
